2bc1513ce17f23d48c974163596bee910db1bb9b
Arjun Malhotra <94740065+ArjunMal1311@users.noreply.github.com>
2024-08-25 12:33:32 +0530

    __

--- components/BFHLForm.tsx
+++ components/BFHLForm.tsx
@@ -63,7 +63,9 @@ export default function BFHLForm() {
             {error && (
                 <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-6" role="alert">
                     <p>{error}</p>
+                    <p>Check for curly brackets</p>
                     <p>Please make sure you have correct inverted commas &quot;M&quot; not “M”</p>
+                    
                 </div>
             )}
 
